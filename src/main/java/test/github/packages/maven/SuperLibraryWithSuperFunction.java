3a1b83f19395855d431850a03de424603d5d1a24
George <g.zougianos@gmail.com>
2021-07-10 21:13:03 +0300

    say an update in the same version

--- src/main/java/test/github/packages/maven/SuperLibraryWithSuperFunction.java
+++ src/main/java/test/github/packages/maven/SuperLibraryWithSuperFunction.java
@@ -5,4 +5,9 @@ public class SuperLibraryWithSuperFunction {
 	public static double calculateLife() {
 		return -0.000000000000000001d;
 	}
+	
+	
+	public static double calculateDeath() {
+		return -0.000000000000000001d;
+	}
 }
