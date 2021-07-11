574cd6f99fe5da8ad10615a69bd16a87b82664ef
George <g.zougianos@gmail.com>
2021-07-11 18:14:11 +0300

    uploaded mvn with parent to test deploy

---
+++ with-parent-pom/module1/src/main/java/gh/packages/test/FromMvnModule1.java
@@ -0,0 +1,8 @@
+package gh.packages.test;
+
+public class FromMvnModule1 {
+
+	public static void aGlitchInTheMatrix() {
+
+	}
+}
---
+++ with-parent-pom/module2/src/main/java/gh/packages/maven/FromMvnModule2.java
@@ -0,0 +1,8 @@
+package gh.packages.maven;
+
+public class FromMvnModule2 {
+
+	public static void drinkWaterItsGoodForYourHealth() {
+
+	}
+}
